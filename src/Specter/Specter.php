53e62015e4572fa26f7ceab90c2d6d8ed4271f92
Christopher Studer <christopher@reinetworklp.com>
2017-10-01 13:35:36 -0500

    hmm

--- src/Specter/Specter.php
+++ src/Specter/Specter.php
@@ -1,13 +1,30 @@
 <?php
 namespace Specter;
 
+use Specter\View;
+use Specter\DB;
+
 class Specter
 {
     protected $settings = [];
 
     public function __construct(array $settings = [])
     {
-        $this->settings = $settings;
+        //TODO cache settings.
+        if (!empty($settings)) {
+            $this->settings = $settings;
+        } else {
+            $configPaths = [
+                'config.php',
+                '../config.php',
+            ];
+            foreach ($configPaths as $path) {
+                if (is_file($path)) {
+                    $this->settings = include $path;
+                    break;
+                }
+            }
+        }
         $this->defaults();
     }
 
@@ -21,6 +38,9 @@ class Specter
         }
         if (!isset($this->settings['webBase'])) {
             $this->settings['webBase'] = '/';
+        }
+        if (!isset($this->settings['configPath'])) {
+            $this->settings['configPath'] = '../config.php';
         }
         if (!isset($this->settings['dbs'])) {
             $this->settings['dbs'] = [
@@ -45,5 +65,63 @@ class Specter
     public function set($name, $value)
     {
         $this->settings[$name] = $value;
+    }
+
+    public function haunt()
+    {
+        session_start();
+        $db = new DB($this);
+        $pdo = $db->pdo();
+        foreach($pdo->query('SELECT * FROM dataSrc') as $row) {
+            print_r($row);
+        }
+
+        $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+            $r->addRoute('GET', '/', 'Main@index');
+        });
+
+        // Fetch method and URI from somewhere
+        $httpMethod = $_SERVER['REQUEST_METHOD'];
+        $uri = $_SERVER['REQUEST_URI'];
+
+        // Strip query string (?foo=bar) and decode URI
+        if (false !== $pos = strpos($uri, '?')) {
+            $uri = substr($uri, 0, $pos);
+        }
+        $uri = rawurldecode($uri);
+
+        $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+        switch ($routeInfo[0]) {
+            case FastRoute\Dispatcher::NOT_FOUND:
+                http_response_code(404);
+                $view = new View($this);
+                echo $view->read('404.php');
+                break;
+            case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = $routeInfo[1];
+                http_response_code(405);
+                $view = new View($this);
+                echo $view->read('405.php');
+                break;
+            case FastRoute\Dispatcher::FOUND:
+                $handler = $routeInfo[1];
+                $vars = $routeInfo[2];
+                $controller = new \App\Controllers\Main($this);
+                echo $controller->index();
+                break;
+        }
+
+        /*
+        set_exception_handler('uncaught_exception_handler');
+        function uncaught_exception_handler($e) {
+          ob_end_clean(); //dump out remaining buffered text
+          $vars['message']=$e;
+          die(View::do_fetch(APP_PATH.'errors/exception_uncaught.php',$vars));
+        }
+        function custom_error($msg='') {
+          $vars['msg']=$msg;
+          die(View::do_fetch(APP_PATH.'errors/custom_error.php',$vars));
+        }
+         */
     }
 }
