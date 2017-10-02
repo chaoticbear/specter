3977b8f853b981f6968847b7c45ac54cbcd6e1a4
Christopher Studer <christopher@reinetworklp.com>
2017-10-02 17:27:06 -0500

    hmm

--- src/Specter/Model.php
+++ src/Specter/Model.php
@@ -14,39 +14,71 @@ abstract class Model
     protected $tbl;
     protected $pk = 'id';
     protected $mods = [];
+    protected $rs = [];
 
     public function __construct(Specter $specter)
     {
         $this->specter = $specter;
         $this->db = $specter->db->pdo($this->con);
+        $this->mods = [];
+    }
+
+    protected function quote($str)
+    {
+        switch($this->db->getAttribute(\PDO::ATTR_DRIVER_NAME)) {
+            case 'sqlsql':
+            case 'mssql':
+            case 'dblib':
+                return '[' . $str . ']';
+                break;
+            case 'mysql':
+                return '`' . $str . '`';
+                break;
+            default:
+                return '"' . $str . '"';
+                break;
+        }
+    }
+
+    public function get($key)
+    {
+        return $this->rs[$key];
     }
 
     public function __get($key)
     {
-        return $this->$key;
+        return $this->get($key);
+    }
+
+    public function set($key, $val)
+    {
+        $this->mods[$key] = $val;
+        $this->rs[$key] = $val;
+        return $this;
     }
 
     public function __set($key, $val)
     {
-        $this->mods[$key] = $val;
-        $this->$key = $val;
-        return $this;
+        return $this->set($key, $val);
     }
 
     public function one($id)
     {
-        $stm = $this->db->prepare('SELECT * FROM ? WHERE ? = ?');
-        $stm->execute([$this->tbl, $this->pk, $id]);
+        $stm = $this->db->prepare('SELECT * FROM ' . $this->quote($this->tbl) .
+            ' WHERE ' . $this->quote($this->pk) . ' = ?');
+        $stm->execute([$id]);
         return $stm->fetchObject(get_class($this), [$this->specter]);
     }
 
     public function delete($id = null)
     {
         if ($id === null) {
-            $id = $this->$$this->pk;
+            $pk = $this->pk;
+            $id = $this->$pk;
         }
-        $stm = $this->db->prepare('DELETE FROM ? WHERE ? = ?');
-        $stm->execute([$this->tbl, $this->pk, $id]);
+        $stm = $this->db->prepare('DELETE FROM ' . $this->quote($this->tbl) .
+            ' WHERE '. $this->quote($this->pk) . ' = ?');
+        $stm->execute([$id]);
         return $stm->rowCount();
     }
 
@@ -55,18 +87,17 @@ abstract class Model
         if (!empty($this->mods)) {
             $s = '';
             foreach ($this->mods as $k => $v) {
-                $s .= ',?=?';
+                $s .= ',' . $this->quote($k) . '=?';
             }
             $s = substr($s,1);
-            $s = 'UPDATE ? SET ' . $c . ' WHERE ? = ?';
+            $s = 'UPDATE ' . $this->quote($this->tbl) . ' SET ' . $s .
+                ' WHERE ' . $this->quote($this->pk) . ' = ?';
             $p = [];
-            $p[] = $this->tbl;
             foreach ($this->mods as $k => $v) {
-                $p[] = $k;
                 $p[] = $v;
             }
-            $p[] = $this->pk;
-            $p[] = $this->$$this->pk;
+            $pk = $this->pk;
+            $p[] = $this->$pk;
             $stm = $this->db->prepare($s);
             $stm->execute($p);
             $this->mods = [];
