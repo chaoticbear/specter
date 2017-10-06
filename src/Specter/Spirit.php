e21f7a2994b405fec4f5dfbf1e060196e47bcc36
Christopher Studer <christopher@reinetworklp.com>
2017-10-06 07:57:00 -0500

    hmm

--- src/Specter/Spirit.php
+++ src/Specter/Spirit.php
@@ -9,11 +9,15 @@ abstract class Spirit
     protected $specter;
     protected $layout = 'default';
     protected $params = [];
+    protected $post = [];
+    protected $get = [];
 
     public function __construct(Specter $specter, $params = [])
     {
         $this->specter = $specter;
         $this->params = $params;
+        $this->post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        $this->get = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
     }
 
     protected function render($file, array $vars = []) {
